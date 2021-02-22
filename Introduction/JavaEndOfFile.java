f5ac346309bac3c9799249bf1cd3022d7a825940
Pedro Paulo <pedroppaulo42@gmail.com>
2021-02-22 20:04:51 -0300

    Java End-of-file

---
+++ Introduction/JavaEndOfFile.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaEndOfFile {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int i = 1;
+
+		while (scan.hasNext()) {
+			System.out.println(i + " " + scan.nextLine());
+			i++;
+		}
+
+		scan.close();
+	}
+}
